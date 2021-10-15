f18b506148c5a40d2c4882301af55b2c77c30821
Justin Peisker <justinpeisker@gmail.com>
2021-10-15 07:11:53 -0700

    initial setup finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import * as yup from 'yup';
+
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  pepperoni: false,
+  sausage: false,
+  mushroom: false,
+  pepper: false,
+  special:  '',
+}
+
+const initialOrder = [];
 
 const App = () => {
+
+  const[order, setOrder] = useState(initialOrder);
+  
   return (
     <>
       <h1>Lambda Eats</h1>
